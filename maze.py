3525f821c68fee841cb9ab653de53cc2c3ac71e4
kelechi <oparaiskelechi@gmail.com>
2024-12-15 01:35:31 +0100

    print of final maze

--- maze.py
+++ maze.py
@@ -1,7 +1,64 @@
 class Maze:
+    
     def __init__(self, filename):
+        self.solution = None
         
         # Read file and set height and width of maze
         with open(filename) as maze_file:
             contents = maze_file.read()
+            
+            # Validate start and goal
+            if contents.count("A") != 1:
+                raise Exception("maze must have exactly one start point")
+            if contents.count("B") != 1:
+                raise Exception("maze must have exactly one goal")
+            
+            # Determine height and width of maze
+            contents = contents.splitlines()
+            self.height = len(contents)
+            self.width = 0
+            for line in contents:
+                if len(line) > self.width:
+                    self.width = len(line)
+            
+            # Keep track of walls
+            self.wall = []
+            for i in range(self.height):
+                row = []
+                for j in range(self.width):
+                    try:
+                        if contents[i][j] == "A":
+                            self.start = (i, j)
+                            row.append(False)
+                        elif contents[i][j] == "B":
+                            self.goal = (i, j)
+                            row.append(False)
+                        elif contents[i][j] == " ":
+                            row.append(False)
+                        else:
+                            row.append(True)
+                    except IndexError:
+                        row.append(False)
+                self.wall.append(row)
+            
+            
+    
+    def print(self):
+        solution = self.solution[1] if self.solution is not None else None
+        print()
+        for i, row in enumerate(self.wall):
+            for j, col in enumerate(row):
+                if col:
+                    print("█", end="")
+                elif (i, j) == self.start:
+                    print("A", end="")
+                elif (i, j) == self.goal:
+                    print("B", end="")
+                elif solution is not None and (i, j) in solution:
+                    print("*", end="")
+                else:
+                    print(" ", end="")  
+            print()
+        print()
+                        
         
--- search.py
+++ search.py
@@ -5,7 +5,9 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: Python3 maze.py maze1.txt")
     
-    m = maze.Maze()
+    m = maze.Maze(sys.argv[1])
+    m.print()
+    # print(f"we are here {m}")
 
 if __name__ == "__main__":
     main()
